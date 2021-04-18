b2ffb334f70e9c73103f703aa177f90e4ccc0f63
Dasha Lary <dasha4988@gmail.com>
2021-04-18 16:20:08 -0400

    added code

--- main.c
+++ main.c
@@ -4,6 +4,17 @@
 
 int main()
 {
+    double width = 32.3;
+    double height = 15.3;
+    double perimeter = 0.0;
+    double area = 0.0;
+
+    perimeter = 2.0 * (height + width);
+    area = width * height;
+
+    printf("Perimeter is %f\n", perimeter);
+    printf("Area is %f\n", area);
+    printf("Width is %f, Height is %f\n", width, height);
 
     return 0;
 }
