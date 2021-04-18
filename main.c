fe909a874243017ae252b7573fdd85a2a111b7a1
Dasha Lary <dasha4988@gmail.com>
2021-04-18 16:21:20 -0400

    added comments

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 
 int main()
 {
+    // declaring variables
     double width = 32.3;
     double height = 15.3;
     double perimeter = 0.0;
     double area = 0.0;
 
+    // doing the math
     perimeter = 2.0 * (height + width);
     area = width * height;
 
+    // printing the values
     printf("Perimeter is %f\n", perimeter);
     printf("Area is %f\n", area);
     printf("Width is %f, Height is %f\n", width, height);
